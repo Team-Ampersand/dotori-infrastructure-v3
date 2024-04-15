cbdae3b21e90e62e0fe7884240bc5a8a11a53769
aesperer <piyrw9754@gmail.com>
2024-04-15 11:43:05 +0900

    add igw, natgatway;

---
+++ nat_gateway.tf
@@ -0,0 +1,17 @@
+resource "aws_nat_gateway" "gw1" {
+  allocation_id = aws_eip.nat1.id
+  subnet_id     = aws_subnet.public_1.id
+
+  tags = {
+    Name = "Dotori NAT 1"
+  }
+}
+
+resource "aws_nat_gateway" "gw2" {
+  allocation_id = aws_eip.nat2.id
+  subnet_id     = aws_subnet.public_2.id
+
+  tags = {
+    Name = "Dotori NAT 2"
+  }
+}
